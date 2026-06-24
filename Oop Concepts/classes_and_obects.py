72c9de4f10694e04331ebcb574c5e99c373f3f35
MZaid2292 <nizamanizaid99@gmail.com>
2026-06-24 10:30:18 +0500

    Inheritance

--- Oop Concepts/classes_and_obects.py
+++ Oop Concepts/classes_and_obects.py
@@ -4,15 +4,15 @@
 
 # Object: An object is an instance of a class. It is a real entity created from a class.
 
-# class Student:
-#     pass
+class Student:
+    pass
 
-# s1 = Student()  #Object
-# s1.name = "Muhammad Zaid"
-# s1.age = 23
+s1 = Student()  #Object
+s1.name = "Muhammad Zaid"
+s1.age = 23
 
-# print(s1.name)
-# print(s1.age)
+print(s1.name)
+print(s1.age)
 
 
 
@@ -20,126 +20,126 @@
 # self refers to the current object of the class. It is used to access the
 #  object's variables and methods.
 
-# class Student:
-#     def __init(self,name):
-#         self.name = name
+class Student:
+    def __init(self,name):
+        self.name = name
 
 
 # What is __init__
 # __init__() is a constructor in Python. It is automatically called when an object is created.
 
-# class Student:  #Class
-#     def __init__(self,name):  #Constructor
-#         self.name = name
+class Student:  #Class
+    def __init__(self,name):  #Constructor
+        self.name = name
 
-# s1 = Student("Muhammad Zaid")  #Object
-# print(s1.name)
+s1 = Student("Muhammad Zaid")  #Object
+print(s1.name)
 
 
 # What is a method
 # A method is a function defined inside a class.
 
-# class Student:
-#     def greet(self):
-#         print("Hello Muhammad Zaid")
+class Student:
+    def greet(self):
+        print("Hello Muhammad Zaid")
 
-# s1 = Student()
-# s1.greet()
-
-
-# class Student:
-#     def __init__(self,name):
-#         self.name = name
-
-#     def greet(self):
-#         print("Hello " + self.name)
-
-# s1 = Student("Muhammad Zaid")
-# s1.greet()
+s1 = Student()
+s1.greet()
 
 
-# class Student:
-#     def __init__(self,name,age):
-#         self.name = name
-#         self.age = age
+class Student:
+    def __init__(self,name):
+        self.name = name
 
-#     def greet(self):
-#         print("Hello " , self.name)
+    def greet(self):
+        print("Hello " + self.name)
 
-#     def show_age(self):
-#         print("You're ", self.age, "years old")
+s1 = Student("Muhammad Zaid")
+s1.greet()
 
-# s1 = Student("Muhammad Zaid",23)
-# s1.greet()
-# s1.show_age()
+
+class Student:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print("Hello " , self.name)
+
+    def show_age(self):
+        print("You're ", self.age, "years old")
+
+s1 = Student("Muhammad Zaid",23)
+s1.greet()
+s1.show_age()
 
 
 # Practice Questions
 # 1. 
-# class Car:
-#     def __init__(self,brand):
-#         self.brand = brand
+class Car:
+    def __init__(self,brand):
+        self.brand = brand
 
-#     def show_brand(self):
-#         print("The Car brand is", self.brand)
+    def show_brand(self):
+        print("The Car brand is", self.brand)
 
-# car = Car("Toyota")
-# car.show_brand()
+car = Car("Toyota")
+car.show_brand()
 
 
-# class Student:
-#     def __init__(self,name,age):
-#         self.name = name
-#         self.age = age
+class Student:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
 
-#     def show_info(self):
-#         # print("Name: ", self.name, "\nAge: ",self.age )   
-#         print(f"Name: {self.name}, Age: {self.age} ") 
+    def show_info(self):
+        # print("Name: ", self.name, "\nAge: ",self.age )   
+        print(f"Name: {self.name}, Age: {self.age} ") 
 
-# s1 = Student("Muhammad Zaid", 23)
-# s1.show_info()
+s1 = Student("Muhammad Zaid", 23)
+s1.show_info()
 
 
 # Question 1: Print Car brand and color by using show_info method
 
-# class Car:
-#     def __init__(self,brand,color):
-#         self.brand = brand
-#         self.color = color
+class Car:
+    def __init__(self,brand,color):
+        self.brand = brand
+        self.color = color
 
-#     def show_info(self):
-#         print(f"Brand: {self.brand} \nColor: {self.color}")
+    def show_info(self):
+        print(f"Brand: {self.brand} \nColor: {self.color}")
 
-# car = Car("Toyota Corolla", "Black Color")
-# car.show_info()   
+car = Car("Toyota Corolla", "Black Color")
+car.show_info()   
 
 # Question 2: Employee Class Requirements: Name and Salary
-# class Employee:
-#     def __init__(self,name,salary):
-#         self.name = name
-#         self.salary = salary
+class Employee:
+    def __init__(self,name,salary):
+        self.name = name
+        self.salary = salary
 
-#     def show_detail(self):
-#         print(f"Name: {self.name} \nSalary: {self.salary}")
+    def show_detail(self):
+        print(f"Name: {self.name} \nSalary: {self.salary}")
 
-# employee = Employee("Muhammad Zaid", 100000)
-# employee.show_detail()
+employee = Employee("Muhammad Zaid", 100000)
+employee.show_detail()
 
 # Question Calculator Class Requirements 2 number(a,b) method add() multiply
-# class Calculator:
-#     def __init__(self, a , b):
-#         self.a = a
-#         self.b = b
+class Calculator:
+    def __init__(self, a , b):
+        self.a = a
+        self.b = b
 
-#     def add(self):
-#         print(f"Sum: {self.a + self.b}")
+    def add(self):
+        print(f"Sum: {self.a + self.b}")
 
-#     def multiply(self):
-#         print(f"Multiply: {self.a * self.b}")
+    def multiply(self):
+        print(f"Multiply: {self.a * self.b}")
 
-# calculator = Calculator(10, 5)
-# calculator.add()
-# calculator.multiply()
+calculator = Calculator(10, 5)
+calculator.add()
+calculator.multiply()
 
 class StudentMarks:
     def __init__(self,name,marks):
--- Oop Concepts/inheritance.py
+++ Oop Concepts/inheritance.py
@@ -1,17 +1,227 @@
-# Inheritance: Inheritance allows us to define a class that inherits all the methods
-#  and properties from another class.
+# Inheritance: Inheritance allows a child class to use the attributes and methods of a parent class.
 
-class Animal:
-    def eat(self):
-        print("Eating")
+# class Animal:
+#     def eat(self):
+#         print("Eating")
 
-    def sleep(self):
-        print("Sleeping")
+#     def sleep(self):
+#         print("Sleeping")
 
 
-class Dog(Animal):
-    pass
+# class Dog(Animal):
+#     pass #
 
-d = Dog()
-d.eat()
-d.sleep()
+# d = Dog()
+# d.eat()
+# d.sleep()
+
+# --------------------------- Practice Questions-------------------------------------------
+# 1. 
+# class Person:
+
+#     def walk(self):
+#         print("walking")
+
+# class Student(Person):
+#     pass
+
+# s1 = Student()
+# s1.walk()
+
+
+# 2.
+# class Vehicle:
+
+#     def start(self):
+#         print("Vehicle Started")
+
+# class Car(Vehicle):
+#     pass
+
+# car = Car()
+# car.start()
+
+# 3. 
+# class Animal:
+
+#     def eat(self):
+#         print("Eating")
+
+# class Dog(Animal):
+
+#     def sound(self):
+#         print("Dog is barking")
+
+# d = Dog()
+# d.eat()
+# d.sound()
+
+# 4. 
+# class Person:
+#     def __init__(self,name):
+#         self.name = name
+
+#     def print_name(self):
+#         print("Hello ", self.name)
+
+# class Student(Person):
+#     pass
+
+# s1 = Student("Muhammad Zaid")
+# s1.print_name()
+
+# 5. 
+# class Employee:
+#     def __init__(self,name,salary):
+#         self.name = name
+#         self.salary = salary
+
+#     def show_details(self):
+#         print(f"Name: {self.name} \nSalary: {self.salary}") 
+
+# class Manager(Employee):
+#     pass
+
+# manager = Manager("Muhammad Zaid", 100000)
+# manager.show_details()
+
+# 6. 
+# class BankAccount:
+#     def __init__(self,account_holder):
+#         self.account_holder = account_holder
+
+#     def name_account_holder(self):
+#         print("Account Holder: ", self.account_holder)
+
+# class SavingAccount(BankAccount):
+#     pass
+
+# savingaccount = SavingAccount("Muhammad Zaid")
+# savingaccount.name_account_holder()
+
+
+# -------------------------------------------Method Overriding Examples----------------------
+
+# 1.
+# class Animal:
+#     def sound(self):
+#         print("Animal makes a sound")
+
+# class Dog(Animal):
+#     def sound(self):
+#         print("Dog Barks")
+
+# d = Dog()
+# d.sound()
+
+# 2.
+# class Vehicle:
+#     def start(self):
+#         print("Vehicle started")
+
+# class Car(Vehicle):
+#     def start(self):
+#         print("Car started")
+
+# c = Car()
+# c.start()
+
+# 3. 
+# class Person:
+#     def intro(self):
+#         print("I am a person")
+
+# class Student(Person):
+#     def intro(self):
+#         print("I am a student")
+
+# s1 = Student()
+# s1.intro()        
+
+
+# 4.
+# class Employee:
+#     def __init__(self,name):
+#         self.name = name
+#     def show_role(self):
+#         print(f"Employee Name: {self.name}")
+
+# class Manager(Employee):
+#     def show_role(self):
+#         print(f"Manager: {self.name}")
+
+# manager = Manager("Muhammad Zaid")
+# manager.show_role()
+
+
+# --------------------------------------Super keyword---------------------------------------
+
+# class Person:
+#     def __init__(self,name,age,profession):
+#         self.name = name
+#         self.age = age
+#         self.profession = profession
+
+  
+
+# class Student(Person):
+#     def __init__(self,name,age,profession,cgpa):
+#         super().__init__(name,age,profession)
+#         self.cgpa = cgpa
+
+#     def show_detail(self):
+#         print(f"Name: {self.name} \nAge: {self.age} \nProfession: {self.profession} \nCGPA: {self.cgpa}")
+
+# s1 = Student("Muhammad Zaid",23,"Student",3.2)
+# s1.show_detail()
+
+
+# 1.
+# class Employee:
+#     def __init__(self,name,salary):
+#         self.name = name
+#         self.salary = salary
+
+# class Manager(Employee):
+#     def __init__(self,name,salary,department):
+#         super().__init__(name,salary)
+#         self.department = department
+
+#     def show_detail(self):
+#         print(f"Name: {self.name} \nSalary: {self.salary} \nDepartment: {self.department}")
+
+# manager = Manager("Muhammad Zaid", 100000, "Development")
+# manager.show_detail()
+
+# 2.
+# class Product:
+#     def __init__(self,name,price):
+#         self.name = name
+#         self.price = price
+
+# class Mobile(Product):
+#     def __init__(self,name,price,brand):
+#         super().__init__(name,price)
+#         self.brand = brand
+
+#     def show_details(self):
+#         print(f"Name: {self.name} \nPrice: {self.price} \nBrand: {self.brand}")
+
+# mob = Mobile("Pixel 7", 65000, "Google")
+# mob.show_details()
+
+class BankAccount:
+    def __init__(self, holder, balance):
+        self.holder = holder
+        self.balance = balance
+
+class SavingsAccount(BankAccount):
+    def __init__(self, holder, balance, interest):
+        super().__init__(holder,balance)
+        self.interest = interest
+
+    def show(self):
+        print(self.holder, self.balance, self.interest)
+
+acc = SavingsAccount("Zaid", 5000, 5)
+acc.show()
