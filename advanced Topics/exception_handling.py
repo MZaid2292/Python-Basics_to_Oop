56c9ba863c95e76eb09c9e365feec9828bac3bb2
MZaid2292 <nizamanizaid99@gmail.com>
2026-06-29 11:23:22 +0500

    Exception handly topic finally

--- advanced Topics/exception_handling.py
+++ advanced Topics/exception_handling.py
@@ -1,14 +1,14 @@
 
-# Exception: Exception is an error that comes during execution of program.
+# # Exception: Exception is an error that comes during execution of program.
 
-# Exception Q handle karty hain? Like we have a code
+# # Exception Q handle karty hain? Like we have a code
  
 # num = int(input("Enter Number: ")) #hamne input abc diya
 # print(num) 
 # print("Program End")
 
-# Output: value error aega because ham integer input le rahe hain or de string rahe hain
-# Program wahi stop hojaega lekin if ham exception handling use karen to 
+# # Output: value error aega because ham integer input le rahe hain or de string rahe hain
+# # Program wahi stop hojaega lekin if ham exception handling use karen to 
 
 # try:
 #     num = int(input("Enter Number: "))
@@ -19,11 +19,11 @@
 # print("program end")
 
 
-# Try: is code ko try karo. Agr error ae to program stop nh karo 
-# Except: Agr error ae to ye code chala do 
+# # Try: is code ko try karo. Agr error ae to program stop nh karo 
+# # Except: Agr error ae to ye code chala do 
 
-# Example:
-# 1.
+# # Example:
+# # 1.
 # try:
 #     number = int(input("Enter Number: "))
 #     print(number)
@@ -31,7 +31,7 @@
 # except:
 #     print("Please Enter a valid number")
 
-# 2.
+# # 2.
 # try:
 #     a = 10
 #     b = 0
@@ -39,7 +39,7 @@
 # except:
 #     print("Can not divide by zero")    
 
-# 3.
+# # 3.
 # try:
 #     numbers = [10,20,30]
 #     print(numbers[5])
@@ -47,7 +47,7 @@
 #     print("Index doesn't exist.")    
 
 
-# 4. Multiple Exceptions
+# # 4. Multiple Exceptions
 # try:
 #     num1 = int(input("Enter first number"))
 #     num2 = int(input("Enter second number"))
@@ -67,11 +67,11 @@
 #     print(e) 
 
 
-# except: → Catches the error, but does not give you the actual error message.
-# except Exception as e: → Catches the error and stores the actual error message in e.
+# # except: → Catches the error, but does not give you the actual error message.
+# # except Exception as e: → Catches the error and stores the actual error message in e.
 
 
-# -------------------------------Practice Questions----------------------------------------------
+# # -------------------------------Practice Questions----------------------------------------------
 # 1.
 # try:
 #     age = int(input("Enter your age: "))
@@ -80,7 +80,7 @@
 # except:
 #     print("Please enter a valid age")
 
-# 2.
+# # 2.
 # try:
 #     num1 = int(input("Enter First Number: "))
 #     num2 = int(input("Enter Second Number: "))
@@ -91,7 +91,7 @@
 # except ZeroDivisionError:
 #     print("Cannot divide zero")
 
-# 3.
+# # 3.
 # try:
 #     numbers = [10,20,30,40]
 #     index = int(input("Enter Index"))
@@ -140,34 +140,162 @@
 
 
 
-#The else block executes only if no exception occurs in the try block.
+# # The else block executes only if no exception occurs in the try block.
     
-# try:
-#     subjects = ["Python","Java","Flutter","React"]
 
-#     index = int(input("Enter index:"))
-#     print("Selected Subject:", subjects[index])
+
+# # try:
+# #     subjects = ["Python", "Java", "Flutter", "React"]
+
+# #     index = int(input("Enter index:"))
+# #     subject = subjects[index]
+
+# # except ValueError:
+# #     print("Please enter a valid number.")
+
+# # except IndexError:
+# #     print("Subject not found.")
+
+# # else:
+# #     print("Selected Subject:", subject)
+    
+
+
+# # Finally Keyword: The finally block always executes, whether an exception occurs or not.
+
+
+# 1.
+# try:
+#     student = {
+#         "name":"Muhammad Zaid",
+#         "age":23,
+#         "profession":"Software Engineer"
+#     }
+
+#     key = input("Enter Key: ")
+#     print(student[key])
+
+# except KeyError:
+#     print("Please enter a valid key")
+
+# finally:
+#     print("Program Finished")
+
+
+# 2.
+# try:
+#     id = int(input("Enter Student ID: "))
+    
 
 # except ValueError:
-#     print("Subject not found")
+#     print("Invalid Student ID.")
+
+# else:
+#     print("Login Successful")
+#     print("Student ID: ",id)
+
+# finally:
+#     print("Session Closed")
+
+
+# 3.
+# try:
+#     products = {
+#         "laptop":120000,
+#         "mouse":2500,
+#         "keyboard":5000
+#     }
+
+#     product_name = input("Enter a product name: ")
+#     product = products[product_name]
+
+# except KeyError:
+#     print("Product not available")
+
+# else:
+#     print("Good choice")
+#     print("Your selected product is: ",product)
+
+# finally:
+#     print("Thank you for visiting our store")
+
+
+# 3.
+# try:
+#     books = ["Python","Flutter","Java","React"]
+
+#     index = int(input("Enter an index"))
+#     book = books[index]
+
+# except ValueError:
+#     print("Only numbers are allowed")
 
 # except IndexError:
-#     print("Index not found")
+#     print("Index doesn't exist. ")
+
+# else:
+#     print("Selected Book is: ",book)
+
+# finally:
+#     print("Library session ended")
 
 
 
+# 4.
+# try:
+#     balance = 100000
+
+#     withdrawl_amount = int(input("Enter Withdrawl Amount: "))
+
+# except ValueError:
+#     print("Invalid Amount")
+
+# else:
+#     if withdrawl_amount <= balance:
+#         print("Withdrawl Amount: ",withdrawl_amount)
+#         remaining_balance = balance - withdrawl_amount
+#         print("Remaining Balance: ",remaining_balance)
+
+#     else:
+#         print("Insufficient Balance")
+
+# finally:
+#     print("Thank you for banking with us.")
+    
+# 5.
 try:
-    subjects = ["Python", "Java", "Flutter", "React"]
+    num1 = int(input("Enter First Number: "))
+    num2 = int(input("Enter second Number: "))
+    operator = input("Enter an operator (+, -, *, /): ")
+    print("Selected Operator: ",operator)
 
-    index = int(input("Enter index:"))
-    subject = subjects[index]
+    if operator == "+":
+        result = num1 + num2
+
+    elif operator == "-":
+        result = num1 - num2
+        
+    elif operator == "*":
+        result = num1 * num2
+        
+    elif operator == "/":
+        result = num1 / num2
+       
+
+    else:
+        print("Invalid Operator")
 
 except ValueError:
-    print("Please enter a valid number.")
+    print("Only numbers are allowed")
 
-except IndexError:
-    print("Subject not found.")
+except ZeroDivisionError:
+    print("Cannot divide by zero")
 
 else:
-    print("Selected Subject:", subject)
+    print("Result: ",result)
     
+
+finally:
+    print("Calculator Closed")
+
+
